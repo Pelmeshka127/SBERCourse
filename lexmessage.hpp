55a70aac9ef12db1d3de7c07a884f1bfcab9cb30
Kirill Pavlovskiy <kirya0703@gmail.com>
2024-03-10 00:38:44 +0300

    Adding argv, first fix in comments

---
+++ lexmessage.hpp
@@ -0,0 +1,16 @@
+#ifndef LEXMESSAGE_HPP_
+#define LEXMESSAGE_HPP_
+
+#include <iostream>
+#include <string>
+#include <cstring>
+
+#define MAX_STRING_LENGTH 255
+
+//====================================================================================================//
+
+void PrintMessage(std::string t_class, std::string t_value);
+
+//====================================================================================================//
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+#include <fstream>
 
 #include <FlexLexer.h>
 
-// here we can return non-zero if lexing is not done inspite of EOF detected
 int yyFlexLexer::yywrap() { return 1; }
 
-int main() 
+int main(int argc, char** argv) 
 {
+    std::ifstream file{};
+
+    file.open(argv[1]);
+
     FlexLexer *lexer = new yyFlexLexer;
-    
-    while (lexer->yylex() != 0) 
+
+    while (lexer->yylex(&file) != 0) 
     {
         // do nothing for now, all is in rules
     }
 
     delete lexer;
+
+    file.close();
 }
